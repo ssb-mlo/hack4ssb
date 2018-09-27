13552d6f36e0346c23eed8ec82b1498057cd2989
Larsen, Glenruben Engen <Glenruben.Larsen@ssb.no>
2018-09-27 14:39:15 +0200

    Legger til boks, mangler logikk for å følge slider.

--- src/components/ui-components/input-slider.js
+++ src/components/ui-components/input-slider.js
@@ -1,6 +1,6 @@
 import 'rc-slider/assets/index.css';
 import 'rc-tooltip/assets/bootstrap.css';
-import React from  'react'
+import React from 'react'
 import {withGlobalState} from "react-globally";
 import Slider from 'rc-slider'
 import Tooltip from 'rc-tooltip'
@@ -8,7 +8,7 @@ import Tooltip from 'rc-tooltip'
 const Handle = Slider.Handle;
 
 const handle = (props) => {
-    const { value, dragging, index, ...restProps } = props;
+    const {value, dragging, index, ...restProps} = props;
     return (
         <Tooltip
             prefixCls="rc-slider-tooltip"
@@ -22,17 +22,61 @@ const handle = (props) => {
     );
 };
 
+class FigureList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedValue: 0,
+        };
+        this.props = {
+            number: 0,
+        }
+    }
+
+
+    renderFigure() {
+
+        return(
+
+            this.figure(this.props.number)
+    )
+    }
+
+    figure(number) {
+        return (
+            <div id={number} className={'flexbox'}>
+                <svg width="400" height="110">
+                    <rect width="100" height="100" className={'testBox'}/>
+                    Sorry, your browser does not support inline SVG.
+                </svg>
+            </div>
+        );
+
+    }
+
+    render() {
+        return (
+            <div>
+
+                {this.renderFigure()}
+            </div>
+        )
+    }
+}
+
 
 class SliderComponent extends React.PureComponent {
 
-
-    render(){
-        return(
+    render() {
+        let sliderMin = 0;
+        let sliderMax = 20;
+        return (
             <div className={'container'}>
                 <div className={'col'}>
-                    <div >
+                    <div>
                         <p>Slider with custom handle</p>
-                        <Slider min={0} max={20} defaultValue={3} handle={handle}/>
+                        <Slider min={sliderMin} max={sliderMax} defaultValue={3} handle={handle} dots={true}/>
+                        <FigureList number='5'/>
                     </div>
                 </div>
             </div>
